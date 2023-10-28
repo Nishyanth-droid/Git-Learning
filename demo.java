990137d7d2000259ce6d6d49ae1be42355742ce2
Nishyanth varun reddy Somagattu <nishyanthvarun17@gmail.com>
2023-10-28 12:23:00 +0530

    Java program to sum elements in array

--- demo.java
+++ demo.java
@@ -1,8 +1,16 @@
+import java.util.*;
 class demo{
      public static void main(String[] args){
-         System.out.println("Hello");
-System.out.println("world");
-
+         int[] nums = {1,3,23,65,8,89};
+        int answer = arraySum(nums);
+        System.out.println("Sum of elements in array " + Arrays.toString(nums) + " is : " + answer);
 }
+public static int arraySum(int[] arr){
+        int sum = 0;
+        for(int x : arr){
+            sum += x;
+        }
+        return sum;
+    }
 
 }
